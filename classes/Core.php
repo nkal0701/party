26b07b695f56d13ad4fad3361af6b8c9d2346669
Alfred Marius Nkemla Lienou <nkemlamarius@gmail.com>
2014-10-02 06:45:25 -0500

    Building FrameWork is over

--- classes/Core.php
+++ classes/Core.php
@@ -1,8 +1,16 @@
 <?php
 class Core
 {
+	public $name = 'Alfred';
+	protected $email = 'dummy@dummydomain.com';
+	private $address = 'some dummy address';
+
 	public function run()
 	{
-		
+		ob_start();
+		require_once(Url::getPage());
+		ob_get_flush();
+
+
 	}
 }
---
+++ pages/error.php
@@ -0,0 +1,5 @@
+<?php require_once('_header.php'); ?>
+
+<h1>Error</h1>
+
+<?php require_once('_footer.php') ?>
